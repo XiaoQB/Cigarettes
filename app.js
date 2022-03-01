a2e126bd6fa29ee265899d2c06173576b40a103b
QuanbinXiao <plus2019@foxmail.com>
2022-03-01 19:47:49 +0800

    demo

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-// app.js
+//app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
   },
-  globalData: {
-    userInfo: null
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData:{
+    userInfo:null
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,112 @@
-// index.js
-// 获取应用实例
-const app = getApp()
+//index.js
 
+var util = require('../../utils/util.js')
+var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    feed: [],
+    feed_length: 0
   },
-  // 事件处理函数
-  bindViewTap() {
+  //事件处理函数
+  bindItemTap: function() {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../answer/answer'
     })
   },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
+  bindQueTap: function() {
+    wx.navigateTo({
+      url: '../question/question'
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    this.getData();
+  },
+  upper: function () {
+    wx.showNavigationBarLoading()
+    this.refresh();
+    console.log("upper");
+    setTimeout(function(){wx.hideNavigationBarLoading();wx.stopPullDownRefresh();}, 2000);
+  },
+  lower: function (e) {
+    wx.showNavigationBarLoading();
+    var that = this;
+    setTimeout(function(){wx.hideNavigationBarLoading();that.nextLoad();}, 1000);
+    console.log("lower")
+  },
+  //scroll: function (e) {
+  //  console.log("scroll")
+  //},
+
+  //网络请求数据, 实现首页刷新
+  refresh0: function(){
+    var index_api = '';
+    util.getData(index_api)
+        .then(function(data){
+          //this.setData({
+          //
+          //});
+          console.log(data);
+        });
+  },
+
+  //使用本地 fake 数据实现刷新效果
+  getData: function(){
+    var feed = util.getData2();
+    console.log("loaddata");
+    var feed_data = feed.data;
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      feed:feed_data,
+      feed_length: feed_data.length
+    });
+  },
+  refresh: function(){
+    wx.showToast({
+      title: '刷新中',
+      icon: 'loading',
+      duration: 3000
+    });
+    var feed = util.getData2();
+    console.log("loaddata");
+    var feed_data = feed.data;
+    this.setData({
+      feed:feed_data,
+      feed_length: feed_data.length
+    });
+    setTimeout(function(){
+      wx.showToast({
+        title: '刷新成功',
+        icon: 'success',
+        duration: 2000
+      })
+    },3000)
+
+  },
+
+  //使用本地 fake 数据实现继续加载效果
+  nextLoad: function(){
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 4000
     })
+    var next = util.getNext();
+    console.log("continueload");
+    var next_data = next.data;
+    this.setData({
+      feed: this.data.feed.concat(next_data),
+      feed_length: this.data.feed_length + next_data.length
+    });
+    setTimeout(function(){
+      wx.showToast({
+        title: '加载成功',
+        icon: 'success',
+        duration: 2000
+      })
+    },3000)
   }
+
+
 })
